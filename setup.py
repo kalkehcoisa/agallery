5882b9d34583b9535bfa925e4b6695c07c14a485
Jayme <kalkehcoisa@gmail.com>
2018-07-11 13:39:57 -0300

    Adjusts to setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,9 @@ with open(os.path.join(here, 'README.txt')) as f:
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-requires = [
-    'boto3==1.7.54',
-    'plaster_pastedeploy==0.5',
-    'pyramid==1.9.2',
-    'pyramid_debugtoolbar==4.4',
-    'pyramid_jinja2==2.7',
-    'pyramid_nacl_session==0.3',
-    'pyramid_retry==0.5',
-    'pyramid_tm==2.2',
-    'SQLAlchemy==1.2.9',
-    'transaction==2.2.1',
-    'zope.sqlalchemy==1.0',
-    'waitress==1.1.0',
-    'WTForms-Alchemy==0.16.7'
-]
+with open(os.path.join(here, 'requirements.txt')) as f:
+    requires = list(map(lambda x: x.strip(), f.readlines()))
+
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
